c5d8769d7a43e41504fd2d00c01972b1784f1d37
Lalith Suresh <lsuresh@vmware.com>
2023-01-10 09:02:59 -0800

    Delete existing demo pod first if it exists
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

--- demo/kind-deploy/login.sh
+++ demo/kind-deploy/login.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Kill any previous demo pod instances first
+kubectl delete pod demo-pod
+
 # Login to the demo pod terminal with an environment variable pointing to the Redpanda brokers
 kubectl run -i --tty demo-pod --image=localhost:5001/dbspdemo --restart=Never --env="REDPANDA_BROKERS=`kubectl get po redpanda-0 --template '{{.status.podIP}}'`:9093" -- bash
