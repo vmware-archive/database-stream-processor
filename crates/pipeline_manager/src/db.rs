feldera/feldera

commit fb7704386c11f5ba8ba553d7445a659d648379d6

Deadlock in pipeline manager.
Fixed a deadlock caused by nested lock acquisition.

crates/pipeline_manager/src/db.rs
@@ -196,9 +196,7 @@ impl ProjectDB {
     /// database created with `create_db.sql` along with access credentials.
     pub(crate) fn connect(config: &ManagerConfig) -> AnyResult<Self> {
         unsafe {
-            rusqlite::trace::config_log(Some(|errcode, msg| {
-                debug!("sqlite: {msg}:{errcode}")
-            }))?
+            rusqlite::trace::config_log(Some(|errcode, msg| debug!("sqlite: {msg}:{errcode}")))?
         };
         let dbclient = Connection::open(config.database_file_path())?;
 

crates/pipeline_manager/src/main.rs
@@ -544,7 +544,7 @@ async fn project_status(state: WebData<ServerState>, req: HttpRequest) -> impl R
 }
 
 /// Request to create a new DBSP project.
-#[derive(Deserialize, ToSchema)]
+#[derive(Debug, Deserialize, ToSchema)]
 struct NewProjectRequest {
     /// Project name.
     #[schema(example = "Example project")]
@@ -602,7 +602,12 @@ async fn do_new_project(
     request: web::Json<NewProjectRequest>,
 ) -> AnyResult<HttpResponse> {
     if request.overwrite_existing {
-        let descr = state.db.lock().await.lookup_project(&request.name)?;
+        let descr = {
+            let db = state.db.lock().await;
+            let descr = db.lookup_project(&request.name)?;
+            drop(db);
+            descr
+        };
         if let Some(project_descr) = descr {
             do_delete_project(state.clone(), project_descr.project_id).await?;
         }
@@ -811,7 +816,10 @@ async fn do_delete_project(
     let db = state.db.lock().await;
 
     for pipeline in db.list_project_pipelines(project_id)?.iter() {
-        state.runner.delete_pipeline(pipeline.pipeline_id).await?;
+        state
+            .runner
+            .delete_pipeline(&db, pipeline.pipeline_id)
+            .await?;
     }
 
     db.delete_project(project_id)
@@ -1318,9 +1326,11 @@ async fn pipeline_delete(state: WebData<ServerState>, req: HttpRequest) -> impl 
         Ok(pipeline_id) => pipeline_id,
     };
 
+    let db = state.db.lock().await;
+
     state
         .runner
-        .delete_pipeline(pipeline_id)
+        .delete_pipeline(&db, pipeline_id)
         .await
         .unwrap_or_else(|e| http_resp_from_error(&e))
 }

crates/pipeline_manager/src/runner.rs
@@ -472,11 +472,15 @@ scrape_configs:
 
     /// Kill the pipeline if it is still running, delete its file system state
     /// and remove the pipeline from the database.
-    pub(crate) async fn delete_pipeline(&self, pipeline_id: PipelineId) -> AnyResult<HttpResponse> {
-        let db = self.db.lock().await;
-
+    // Takes a reference to an already locked DB instance, since this function
+    // is invoked in contexts where the client already holds the lock.
+    pub(crate) async fn delete_pipeline(
+        &self,
+        db: &ProjectDB,
+        pipeline_id: PipelineId,
+    ) -> AnyResult<HttpResponse> {
         // Kill pipeline.
-        let response = self.do_shutdown_pipeline(&db, pipeline_id).await?;
+        let response = self.do_shutdown_pipeline(db, pipeline_id).await?;
         if !response.status().is_success() {
             return Ok(response);
         }

vmware-archive/database-stream-processor

commit 20673c1aeeefa6b6cd71ac900e8a2d5f147579ba

Fix regression introduced in #317.
Correctly parse the new "system_error" status from the database.

crates/pipeline_manager/src/db.rs
@@ -127,6 +127,7 @@ impl ProjectStatus {
                 }
             }
             Some("rust_error") => Ok(Self::RustError(error_string.unwrap_or_default())),
+            Some("system_error") => Ok(Self::SystemError(error_string.unwrap_or_default())),
             Some(status) => Err(AnyError::msg(format!("invalid status string '{status}'"))),
         }
     }