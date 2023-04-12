feldera/feldera

commit 07aa2136b26fddc24a5f9cdd4d77c6910e482d45

Add Postgres support
This patch includes:

* Schema changes to work against both Sqlite and Postgres.
In particular, we use `TEXT` instead of `VARCHAR` because
the sqlx Any driver cannot decode `VARCHAR` responses
from Postgres. For incrementing IDs, we use `AUTOINCREMENT`
in Sqlite and `SERIAL PRIMARY KEY` in Postgres.

* Add parameterized tests for execising the same tests
against both DBs. The Postgres tests needs a Postgres instance to be
deployed and accessible, so we use an environment variable
RUN_POSTGRES_TESTS to be set to 1 for the Postgres tests
to be run. Otherwise, those tests are ignored.

* To connect to Postgres, we can build on Postgres' supported
environment variables, like PGHOST, PGUSER and PGPASSWORD.
This makes it so that we don't have to hard code any of these
in our connection strings.

Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

crates/pipeline_manager/src/config.rs
@@ -23,6 +23,10 @@ fn default_sql_compiler_home() -> String {
     "../sql-to-dbsp-compiler".to_string()
 }
 
+fn default_db_connection_string() -> String {
+    "sqlite".to_string()
+}
+
 /// Pipeline manager configuration read from a YAML config file or from command
 /// line arguments.
 #[derive(Parser, Deserialize, Debug, Clone)]
@@ -94,6 +98,14 @@ pub(crate) struct ManagerConfig {
     #[serde(default)]
     #[arg(long)]
     pub unix_daemon: bool,
+
+    /// Point to a relational database to use for state management.
+    /// Accepted values are "sqlite" or "postgres://<host>:<port>".
+    /// For sqlite, we simply create a DB in the current working directory.
+    /// For postgres, we use the connection string as provided.
+    #[serde(default = "default_db_connection_string")]
+    #[arg(short, long, default_value_t = default_db_connection_string())]
+    pub db_connection_string: String,
 
     /// [Developers only] serve static content from the specified directory.
     /// Allows modifying JavaScript without restarting the server.
@@ -233,15 +245,21 @@ impl ManagerConfig {
 
     /// SQlite database file.
     pub(crate) fn database_connection_string(&self) -> String {
-        let mut s = "sqlite:".to_owned();
-        s.push_str(
-            Path::new(&self.working_directory)
-                // create the DB if it does not already exist
-                .join("manager.db?mode=rwc")
-                .to_str()
-                .unwrap(),
-        );
-        s
+        if "sqlite" == self.db_connection_string {
+            let mut s = "sqlite:".to_owned();
+            s.push_str(
+                Path::new(&self.working_directory)
+                    // create the DB if it does not already exist
+                    .join("manager.db?mode=rwc")
+                    .to_str()
+                    .unwrap(),
+            );
+            s
+        } else if self.db_connection_string.starts_with("postgres://") {
+            self.db_connection_string.clone()
+        } else {
+            panic!("Invalid connection string {}", self.db_connection_string)
+        }
     }
 
     /// Directory where the manager generates Rust crate for the project.

.github/workflows/postgres.yml
@@ -1,0 +1,45 @@
+name: PostgreSQL Integration Test
+on: push
+
+jobs:
+  postgres:
+    runs-on: ubuntu-latest
+
+    services:
+      postgres:
+        image: postgres
+        ports:
+          - 5432:5432
+        env:
+          POSTGRES_PASSWORD: postgres
+        # Set health checks to wait until postgres has started
+        options: >-
+          --health-cmd pg_isready
+          --health-interval 10s
+          --health-timeout 5s
+          --health-retries 5
+
+    steps:
+      - name: Check out repository code
+        uses: actions/checkout@v3
+
+      - name: Install rust
+        uses: actions-rs/toolchain@v1
+        with:
+          profile: minimal
+          toolchain: nightly-2022-11-10
+          default: true
+
+      - name: Restore cache
+        uses: Swatinem/rust-cache@v1
+
+      - name: Run cargo test
+        uses: actions-rs/cargo@v1
+        with:
+          command: test
+          args: db
+        env:
+          PGHOST: localhost 
+          PGUSER: postgres
+          PGPASSWORD: postgres
+          RUN_POSTGRES_TESTS: 1 

crates/pipeline_manager/Cargo.toml
@@ -31,6 +31,8 @@ utoipa-swagger-ui = { version = "3.0.2", features = ["actix-web"] }
 chrono = { version = "0.4.23", default-features = false, features = ["serde"] }
 sqlx = { version = "0.7.0-alpha.1", features = [  "runtime-tokio-native-tls", "sqlite", "postgres", "any" ] }
 tempfile = { version = "3" }
+rstest = "0.15"
+serial_test = "2.0.0"
 
 [target.'cfg(unix)'.dependencies]
 daemonize = { version = "0.4.1" }

vmware-archive/database-stream-processor

commit d400c01c72f167860f9288eeefebda3dc2d63ac8

pipeline_manager: Fix rustdoc errors.
Fixes the following:

```
error: unclosed HTML tag `host`
   --> crates/pipeline_manager/src/config.rs:103:53
    |
103 |     /// Accepted values are "sqlite" or "postgres://<host>:<port>".
    |                                                     ^^^^^^
    |
    = note: `-D rustdoc::invalid-html-tags` implied by `-D warnings`

error: unclosed HTML tag `port`
   --> crates/pipeline_manager/src/config.rs:103:60
    |
103 |     /// Accepted values are "sqlite" or "postgres://<host>:<port>".
    |                                                            ^^^^^^
```

Signed-off-by: Ben Pfaff <bpfaff@vmware.com>
CC: Lalith Suresh <lsuresh@vmware.com>
Fixes: 07aa2136b26f ("Add Postgres support")

crates/pipeline_manager/src/config.rs
@@ -100,7 +100,7 @@ pub(crate) struct ManagerConfig {
     pub unix_daemon: bool,
 
     /// Point to a relational database to use for state management.
-    /// Accepted values are "sqlite" or "postgres://<host>:<port>".
+    /// Accepted values are `sqlite` or `postgres://<host>:<port>`.
     /// For sqlite, we simply create a DB in the current working directory.
     /// For postgres, we use the connection string as provided.
     #[serde(default = "default_db_connection_string")]