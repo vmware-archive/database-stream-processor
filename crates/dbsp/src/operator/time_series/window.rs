85ec0fca7e5de823e2c1cf924734d6cfc79ee118
Leonid Ryzhyk <ryzhyk@gmail.com>
2023-04-14 08:17:59 -0700

    Bounded-memory partitioned rolling aggregate.
    We build the infrastructute to garbage collect partitioned time series
    and apply it to create a bounded-memory version of
    `partitioned_rolling_aggregate`.
    
    Motivation
    ==========
    
    In the previous commit we introduced the infrastructure to truncate time
    series data below a user-provided timestamp.  This mechanism requires
    the input trace to be indexed by time and is therefore not sufficient for
    partitioned time series that are indexed by partition id.  Such
    partitioned time series are used for example in the
    `partitioned_rolling_aggregate` operator, which maintains the trace
    of its output stream.
    
    Design
    ======
    
    To support GC for partitioned time series we introduce a new mechanism
    to truncate the contents of a trace by value:
    
    ```rust
    trait Trace {
        ...
    
        /// Informs the trace that values smaller than `lower_bound` are no longer
        /// used and can be removed from the trace.
        ///
        /// The implementation is not required to remove truncated values instantly
        /// or at all.  This method is just a hint that values below `lower_bound`
        /// are no longer of interest to the consumer of the trace and can be
        /// garbage collected.
        // This API is similar to `BatchReader::truncate_keys_below`, however we make
        // it a method of `trait Trace` rather than `trait BatchReader`.  The difference
        // is that a batch can truncate its keys instanly by simply moving an internal
        // pointer to the first remaining key.  However, there is no similar way to
        // truncate values instantly, this can only be done as part of trace maintenance
        // when either merging or compacting batches.
        fn truncate_values_below(&mut self, lower_bound: &Self::Val);
    }
    ```
    
    The implementation in `Spine` simply records the new bound and only
    drops the truncated values when merging two batches as part of trace
    maintenance.
    
    We use the mechanism to implement a new operator:
    
    ```rust
        pub fn partitioned_rolling_aggregate_with_watermark<PK, TS, V, Agg, PF>(
            &self,
            watermark: &Stream<RootCircuit, TS>,
            partition_func: PF,
            aggregator: Agg,
            range: RelRange<TS>,
        ) -> OrdPartitionedOverStream<PK, TS, Agg::Output, B::R>
    ```
    
    Where the `watermark` stream bounds the out-of-ordedness of the input
    data by providing a monotonically growing lower bound on timestamps
    that can appear in the input stream.  This bound is used to bound the
    amount of state maintained by the operator by truncating its output
    trace.

--- crates/dbsp/src/operator/time_series/window.rs
+++ crates/dbsp/src/operator/time_series/window.rs
@@ -77,9 +77,10 @@ where
         let bound_clone = bound.clone();
         bounds.apply(move |(lower, _upper)| {
             bound_clone.set(lower.clone());
-            ()
         });
-        let trace = self.integrate_trace_with_bound(Some(bound)).delay_trace();
+        let trace = self
+            .integrate_trace_with_bound(bound, TraceBound::new())
+            .delay_trace();
         self.circuit()
             .add_ternary_operator(<Window<B>>::new(), &trace, self, bounds)
     }
@@ -238,10 +239,12 @@ where
 #[cfg(test)]
 mod test {
     use crate::{
-        indexed_zset, operator::{Generator, trace::TraceBound}, zset, Circuit, OrdIndexedZSet, RootCircuit, Runtime, Stream,
+        indexed_zset,
+        operator::{trace::TraceBound, Generator},
+        zset, Circuit, OrdIndexedZSet, RootCircuit, Runtime, Stream,
     };
-    use std::vec;
     use size_of::SizeOf;
+    use std::vec;
 
     #[test]
     fn sliding() {
@@ -451,24 +454,28 @@ mod test {
     fn bounded_memory() {
         let (mut dbsp, input_handle) = Runtime::init_circuit(8, |circuit| {
             let (input, input_handle) = circuit.add_input_zset::<isize, isize>();
-            let bounds = input.watermark_monotonic(|ts| *ts).apply(|ts| (*ts - 1000, *ts));
+            let bounds = input
+                .watermark_monotonic(|ts| *ts)
+                .apply(|ts| (*ts - 1000, *ts));
 
             let bound = TraceBound::new();
             bound.set(isize::max_value());
 
             input.window(&bounds);
 
-            input.integrate_trace_with_bound(Some(bound))
+            input
+                .integrate_trace_with_bound(bound, TraceBound::new())
                 .apply(|trace| {
                     assert!(trace.size_of().total_bytes() < 20000);
                     ()
                 });
 
             input_handle
-        }).unwrap();
+        })
+        .unwrap();
 
-        for i in 0 .. 10000 {
-            for j in i * 100 .. (i + 1) * 100 {
+        for i in 0..10000 {
+            for j in i * 100..(i + 1) * 100 {
                 input_handle.push(j, 1);
             }
             dbsp.step().unwrap();
